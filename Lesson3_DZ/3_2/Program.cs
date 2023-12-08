39048e5d8fabacb5980810455542562c0b5666cb
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-08 12:27:35 +0300

    ДЗ к уроку 3 ошибка сохранения

---
+++ Lesson3_DZ/3_2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте массив на 10 целых чисел, напишите программу, которая определяет к-во целых чисел
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = GenerateRandomArray(10, 1, 100);
+
+        Console.WriteLine("Массив из 10 случайных чисел:");
+        PrintArray(numbers);
+
+        int count = CountEvenNumbers(numbers);
+
+        Console.WriteLine("Количество четных чисел: " + count);
+
+        Console.ReadLine();
+    }
+
+    // Функция для генерации случайного массива
+    static int[] GenerateRandomArray(int size, int min, int max)
+    {
+        int[] arr = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(min, max + 1);
+        }
+
+        return arr;
+    }
+
+    // Функция для вывода массива
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    // Функция для подсчета четных
+    static int CountEvenNumbers(int[] arr)
+    {
+        int count = 0;
+        foreach (int num in arr)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ Lesson3_DZ/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100
+//Найдите к-во элементов массива, значения которых лежат в интервале [20 90]
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+        Random random = new Random();
+
+      
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 100);
+        }
+
+        Console.WriteLine("Массив из 10 случайных чисел:");
+        PrintArray(numbers);
+
+        int count = CountNumbersInRange(numbers, 20, 90);
+
+        Console.WriteLine("Количество чисел в интервале: " + count);
+
+        Console.ReadLine();
+    }
+
+    // Функция для вывода массива
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    // Функция для подсчета чисел
+    static int CountNumbersInRange(int[] arr, int min, int max)
+    {
+        int count = 0;
+        foreach (int num in arr)
+        {
+            if (num >= min && num <= max)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
